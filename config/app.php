3167e50985ffde58956ac92b6185b1be969e32e3
shellus <shellus@endaosi.com>
2016-06-27 16:14:57 +0800

    添加逻辑Log类

--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
         /*
          * Application Service Providers...
          */
+        App\Providers\LogicLogServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
